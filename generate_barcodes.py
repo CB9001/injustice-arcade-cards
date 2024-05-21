2bc4e275658db05c39141a84b8877e4d016a12dc
CB9001 <chrisbridges9001@gmail.com>
2024-05-21 16:05:39 -0400

    Add Barcode Generation Script

---
+++ generate_barcodes.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import code128
+import urllib
+
+card_df = pd.read_csv('card_info.csv')
+
+for index, row in card_df.iterrows():
+    id = row['ID']
+    card_name = row['Character'].replace(' ', '_').replace(':', '_').replace('(', '').replace(')', '')
+    barcode_data = row['Barcode Data']
+    target_image = './barcode_images/%03d_%s_%s.png' % (id, card_name, barcode_data)
+    code128.image(barcode_data).save(target_image)
